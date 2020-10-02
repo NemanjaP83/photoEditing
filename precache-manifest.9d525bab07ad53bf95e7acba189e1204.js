5b75ae73b0ffca3e3341c394803a87d0eec70f29
Nemanja <necaintruder@gmail.com>
2020-10-02 16:05:33 +0200

    Updates

---
+++ precache-manifest.9d525bab07ad53bf95e7acba189e1204.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba46dd727df27c7be338a17833fb69d2",
+    "url": "/photoEditing/index.html"
+  },
+  {
+    "revision": "618abc2586eec51d420b",
+    "url": "/photoEditing/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "18fefd724cfa22f71491",
+    "url": "/photoEditing/static/css/main.0003858f.chunk.css"
+  },
+  {
+    "revision": "618abc2586eec51d420b",
+    "url": "/photoEditing/static/js/2.6f323661.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/photoEditing/static/js/2.6f323661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18fefd724cfa22f71491",
+    "url": "/photoEditing/static/js/main.b5cf02bc.chunk.js"
+  },
+  {
+    "revision": "0d91e23853689d855509",
+    "url": "/photoEditing/static/js/runtime-main.e344e56a.js"
+  },
+  {
+    "revision": "4eee5af78214841444cc5d9eb9248f63",
+    "url": "/photoEditing/static/media/city.4eee5af7.jpg"
+  },
+  {
+    "revision": "40ebe3ad43a627cf07a1d7583ded78fd",
+    "url": "/photoEditing/static/media/man.40ebe3ad.jpg"
+  },
+  {
+    "revision": "00a6037e857de8dd881a7501a656e080",
+    "url": "/photoEditing/static/media/nature.00a6037e.jpg"
+  },
+  {
+    "revision": "5490ac668eb341c20c713ef644a50b83",
+    "url": "/photoEditing/static/media/portrait.5490ac66.jpg"
+  },
+  {
+    "revision": "fa0fa5d24fe153cc46764b3e59932f32",
+    "url": "/photoEditing/static/media/stars.fa0fa5d2.jpg"
+  },
+  {
+    "revision": "aba7cf8b7bddd11b3bab17eebc32a8e1",
+    "url": "/photoEditing/static/media/street.aba7cf8b.jpg"
+  }
+]);
